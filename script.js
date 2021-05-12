22976aaa6b6729fa3a47465184957b0a578f8567
muktialii <70674175+arckermann@users.noreply.github.com>
2021-05-12 14:34:42 +0700

    Upload sintaks
    sintaks

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Ketik: Caka");
+ var b=prompt("Ketik: Ali ganteng");
+  	if(a== "Caka" && b=="Ali ganteng")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
